4d988c639010d7f101744ac48916d00b4bc0a214
Tylah Kapa <1005732@Student.sae.edu.au>
2017-09-10 20:08:19 +1000

    Continuous BGM and Dynamic Lowpassing
    Made a script that changes the low pass filter on a background audio player. The low apss filter lowers when a round is finished, and is also based on health when the round begins.

--- Assets/Game Folder/Scripts/Controls/Player.cs
+++ Assets/Game Folder/Scripts/Controls/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float health = 100.0f;
+    public float maxHealth = 100.0f;
 
 	// Use this for initialization
 	void Start () {
