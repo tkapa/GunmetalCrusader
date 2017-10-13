c989d153c9df29d6e7b25d8fa4e59f7240f02122
Tylah Kapa <1005732@Student.sae.edu.au>
2017-10-13 12:40:27 +1000

    Started Audio Manager

---
+++ Assets/Game Folder/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public GameObject audioPlayerPrefab;
+
+    [Header("AI Audio Clips")]
+    [Space(10)]
+    public AudioClip beginGame;
+
+    public AudioClip endGame;
+
+    [Header("Mech Audio Clips")]
+    [Space(10)]
+    public AudioClip reloading;
+
+    public AudioClip jumping;
+
+    [Header("Enemy Audio Clips")]
+    [Space(10)]
+    public AudioClip enemyDropIn;
+
+    public AudioClip enemyDeath;
+
+    public void SpawnAudioAtPoint(AudioClip clip, Vector3 point)
+    {
+        GameObject a = Instantiate(audioPlayerPrefab, 
+                        point, 
+                        transform.rotation) as GameObject;
+
+        a.GetComponent<AudioPlayer>().PlayClipOnce(clip);
+    }
+}
